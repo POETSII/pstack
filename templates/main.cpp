b3524298b76158395fc49ba5cdedc759b5798f85
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-06-27 16:31:26 +0100

    Add device types to template

--- templates/main.cpp
+++ templates/main.cpp
@@ -3,20 +3,30 @@
 
 #define DEVICE_COUNT 10
 
-class state_t {
+{% for device in graph_type['device_types'] %}
+{% set CLASS_NAME = device['id'] + "_t" %}
+class {{ CLASS_NAME }} {
 
 public:
 
-    int x;
-    int y;
-    int counter;
+    {% for scalar in device['state']['scalars'] %}
+    {{- scalar['type'] }} {{ scalar['name'] }};
+    {% endfor %}
+
+    {{ CLASS_NAME }} (){
+        // Default constructor
+        {% for scalar in device['state']['scalars'] %}
+        {{- scalar['name'] }} = 0;
+        {% endfor %}
+    }
 
     void print() {
-        printf("x = %d\n", this->x);
-        printf("y = %d\n", this->y);
-        printf("counter = %d\n", this->counter);
+        {% for scalar in device['state']['scalars'] %}
+        printf("{{ scalar['name']}} = %d\n", this->{{ scalar['name']}});
+        {% endfor %}
     }
 };
+{% endfor %}
 
 class msg_t {
 
@@ -29,19 +39,11 @@ public:
     }
 };
 
-void init_state(state_t *state) {
-    state->x = 0;
-    state->y = 0;
-    state->counter = 0;
-}
 
-void print_state(state_t *state) {
-}
-
-void receive(state_t *state, msg_t *msg) {
+void receive(node_t *state, msg_t *msg) {
     printf("I received a message\n");
     (*msg).print();
-    state->counter += msg->content;
+    // state->counter += msg->content;
 }
 
 msg_t get_trigger() {
@@ -52,17 +54,13 @@ msg_t get_trigger() {
 
 int main() {
 
-    state_t states[DEVICE_COUNT];
-
-    for (int i=0; i<DEVICE_COUNT; i++)
-        init_state(&states[i]);
-
-    // for (int i=0; i<DEVICE_COUNT; i++)
-    //     print_state(&states[i]);
+    node_t states[DEVICE_COUNT];
 
     msg_t trigger = get_trigger();
 
     receive(&states[0], &trigger);
+
+    states[0].print();
 
     // print_state(&states[0]);
 
