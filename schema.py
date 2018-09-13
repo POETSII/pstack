feea5814fed3e628afaf9ae5b71e2e0eaf4e80c9
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-09-13 16:52:28 +0100

    Refactor schema.py

--- schema.py
+++ schema.py
@@ -1,69 +1,73 @@
+def _build_pin_map(markup):
+    """Build a map: (device id, pin_name) -> pin."""
+
+    result = dict()
+
+    graph_type = markup["graph_type"]
+
+    for device in graph_type["device_types"]:
+        for pin in device['input_pins'] + device['output_pins']:
+            key = (device['id'], pin['name'])
+            result[key] = pin
+
+    return result
+
+
+def _build_device_type_map(markup):
+    """Build a map: device id -> device."""
+
+    device_types = markup["graph_type"]["device_types"]
+    result = {device["id"]: device for device in device_types}
+    return result
+
+
+def _build_pin_index(markup, pin_dir):
+    """Build a map: (device id, pin name) -> index for input/output pins."""
+
+    result = dict()
+    pins_key = 'input_pins' if pin_dir == 'input' else 'output_pins'
+    graph_type = markup["graph_type"]
+
+    for device in graph_type["device_types"]:
+        entries = {
+            (device['id'], pin['name']): ind
+            for ind, pin in enumerate(device[pins_key])
+        }
+        result.update(entries)
+
+    return result
+
+
+def _build_device_index(markup):
+    """Build a map: device id -> index."""
+
+    devices = markup['graph_instance']['devices']
+
+    def get_key(device):
+        return (device['type'], device['id'])
+
+    devices_sorted = sorted(devices, key=get_key)
+
+    return {device['id']: index for index, device in enumerate(devices_sorted)}
+
+
+def _build_device_map(markup):
+
+    devices = markup['graph_instance']['devices']
+
+    return {device['id']: device for device in devices}
+
+
 class Schema(object):
 
     def __init__(self, markup):
-        self._pin_map = self._build_pin_map(markup)
-        self._device_map = self._build_device_map(markup)
-        self._device_type_map = self._build_device_type_map(markup)
-        self._input_pin_index = self._build_pin_index(markup, 'input')
-        self._output_pin_index = self._build_pin_index(markup, 'output')
-        self._device_index = self._build_device_index(markup)
-        self._edge_table = self._build_edge_table(markup)
-
-    def _build_pin_map(self, markup):
-        """Build a map: (device id, pin_name) -> pin."""
-
-        result = dict()
-
-        graph_type = markup["graph_type"]
-
-        for device in graph_type["device_types"]:
-            for pin in device['input_pins'] + device['output_pins']:
-                key = (device['id'], pin['name'])
-                result[key] = pin
-
-        return result
-
-    def _build_device_type_map(self, markup):
-        """Build a map: device id -> device."""
-
-        device_types = markup["graph_type"]["device_types"]
-        result = {device["id"]: device for device in device_types}
-        return result
-
-    def _build_pin_index(self, markup, pin_dir):
-        """Build a map: (device id, pin name) -> index for input/output pins."""
-
-        result = dict()
-        pins_key = 'input_pins' if pin_dir == 'input' else 'output_pins'
-        graph_type = markup["graph_type"]
-
-        for device in graph_type["device_types"]:
-            entries = {
-                (device['id'], pin['name']): ind
-                for ind, pin in enumerate(device[pins_key])
-            }
-            result.update(entries)
-
-        return result
-
-    def _build_device_index(self, markup):
-        """Build a map: device id -> index."""
-
-        devices = markup['graph_instance']['devices']
-
-        def get_key(device):
-            return (device['type'], device['id'])
-
-        devices_sorted = sorted(devices, key=get_key)
-
-        return {device['id']: index for index, device in enumerate(devices_sorted)}
-
-
-    def _build_device_map(self, markup):
-
-        devices = markup['graph_instance']['devices']
-
-        return {device['id']: device for device in devices}
+        self._markup = markup
+        self._pin_map = _build_pin_map(markup)
+        self._device_map = _build_device_map(markup)
+        self._device_index = _build_device_index(markup)
+        self._device_type_map = _build_device_type_map(markup)
+        self._input_pin_index = _build_pin_index(markup, 'input')
+        self._output_pin_index = _build_pin_index(markup, 'output')
 
     def get_pin(self, device_type, pin_name):
         """Return pin given device and pin names."""
@@ -88,19 +92,16 @@ class Schema(object):
 
     def get_device_type(self, device_id):
         """Return device type."""
-
         return self._device_type_map.get(device_id)
 
     def get_device_index(self, device_id):
         """Return device index (unique per device type)."""
-
         return self._device_index[device_id]
 
     def get_device(self, device_id):
-
         return self._device_map[device_id]
 
-    def _build_edge_table(self, markup):
+    def get_edge_table(self):
 
         def get_table_entry(edge):
 
@@ -118,10 +119,6 @@ class Schema(object):
 
             return (src_device_index, dst_device_index, src_pin, dst_pin)
 
-        edges = markup["graph_instance"]["edges"]
+        edges = self._markup["graph_instance"]["edges"]
 
         return map(get_table_entry, edges)
-
-    def get_edge_table(self):
-
-        return self._edge_table
