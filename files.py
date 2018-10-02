88e2fe5b2e577f821f164d274b1249167afe54e3
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-10-02 18:46:43 +0100

    Add write_file to files

--- files.py
+++ files.py
@@ -10,3 +10,9 @@ def read_file(file):
     """Return file content as a string."""
     with open(file, "r") as fid:
         return fid.read()
+
+
+def write_file(file, content):
+    """Write string to file."""
+    with open(file, "w") as fid:
+        fid.write(content)
