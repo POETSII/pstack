0c1ec1424940585ec8e441da5ad6d9b7cdb44302
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-10-15 19:08:30 +0100

    Remove keystone from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 REDIS_HOST="coleridge"
-DAEMON_HOSTS="keystone,aesop,coleridge,byron"
+DAEMON_HOSTS="aesop,coleridge,byron"
 
 time fab -H $DAEMON_HOSTS install setup daemon:"$REDIS_HOST:6379"
