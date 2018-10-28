4e3cb27ce18b52fb1f9100750a3b4704126cfe51
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-10-28 17:25:01 +0000

    Refactor tester.py

--- tester.py
+++ tester.py
@@ -40,34 +40,51 @@ def get_checker_doc_len(checker):
     return len(get_checker_doc(checker))
 
 
-def main():
+def run_tests(sim_result, py_file, verbose=True):
 
+    cfuncs = sorted(load_functions(py_file), key=get_checker_doc_len)
+
+    pass_str = colored("PASS", "green", attrs=["bold"])
+    fail_str = colored("FAIL", "red", attrs=["bold"])
+
+    def cprint(text):
+        if verbose:
+            print text
+
+    for checker in cfuncs:
+        if verbose:
+            put("  - %s ... " % get_checker_doc(checker))
+        try:
+            checker(sim_result)
+            cprint(pass_str)
+        except AssertionError:
+            cprint(fail_str)
+            return False
+
+    return True
+
+
+def get_files():
+    """Return list of (xml, py) file tuples in tests dir."""
     xml_files = [
-        os.path.join("tests", file) for file in os.listdir("tests")
+        os.path.join("tests", file)
+        for file in os.listdir("tests")
         if file.lower().endswith(".xml")
     ]
 
     for xml_file in xml_files:
-
         name, _ = os.path.splitext(xml_file)
         py_file = "%s.py" % name
+        yield (xml_file, py_file)
 
-        cfuncs = sorted(load_functions(py_file), key=get_checker_doc_len)
 
-        pass_str = colored("PASS", "green", attrs=["bold"])
-        fail_str = colored("FAIL", "red", attrs=["bold"])
-
+def main():
+    for xml_file, py_file in get_files():
         print("Simulating %s ... " % colored(xml_file, attrs=["bold"]))
-        simulation_result = psim(read_file(xml_file), {}, {"quiet": True})
-
-        for checker in cfuncs:
-            put("  - %s ... " % get_checker_doc(checker))
-            try:
-                checker(simulation_result)
-                print pass_str
-            except AssertionError:
-                print fail_str
-                sys.exit(1)
+        sim_result = psim(read_file(xml_file), {}, {"quiet": True})
+        test_result = run_tests(sim_result, py_file)
+        if not test_result:
+            sys.exit(1)
 
 
 if __name__ == '__main__':
