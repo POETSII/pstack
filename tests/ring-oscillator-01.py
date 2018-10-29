d0c620ce2d659961ba437ffc3cc2b7b8ab944c18
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-10-29 15:35:46 +0000

    Remove log message unit test functions

--- tests/ring-oscillator-01.py
+++ tests/ring-oscillator-01.py
@@ -1,17 +1,6 @@
-def check_log(results):
-    """Check number of log messages."""
-    assert len(results["log"]) == 40
-
-
 def check_delivered_messages(results):
     """Check number of delivered messages."""
     assert results["metrics"]["Delivered messages"] == 40
-
-
-def check_log_order(results):
-    """Check order of log messages."""
-    device_names = [log_entry[0] for log_entry in results["log"]]
-    assert device_names == ["n0", "n1", "n2", "n3"] * 10
 
 
 def check_states(results):
