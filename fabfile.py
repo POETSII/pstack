ab6c7246897642b0885b5901ef943700a4ce4b10
Ghaith Tarawneh <gtarawneh@gmail.com>
2018-11-01 23:52:09 +0000

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@ from fabric.context_managers import prefix
 
 
 env.use_ssh_config = True
-
+PSTACK_DIR         = "~/psim"
 
 def install():
     """Pull latest master commit.
@@ -14,14 +14,14 @@ def install():
     Clones the repository if it doesn't exist.
     """
 
-    if not exists("~/psim"):
+    if not exists(PSTACK_DIR):
         with cd("~"):
-            run("git clone git@github.com:poetsii/psim")
+            run("git clone git@github.com:poetsii/pstack")
         return
 
     run("killall -q python || true")  # terminate any existing daemons
 
-    with cd("~/psim"):
+    with cd(PSTACK_DIR):
         run("git fetch --all >/dev/null")
         run("git reset --hard origin/master >/dev/null")
 
@@ -29,12 +29,12 @@ def install():
 def setup():
     """Setup virtualenv and install dependencies."""
 
-    if not exists("~/psim"):
-        raise Exception("psim is not installed")
+    if not exists(PSTACK_DIR):
+        raise Exception("pstack is not installed")
 
     run("killall -q python || true")  # terminate any existing daemons
 
-    with cd("~/psim"):
+    with cd(PSTACK_DIR):
         run("virtualenv env")
         run("source env/bin/activate && pip install -q -r requirements.txt")
 
@@ -45,7 +45,7 @@ def daemon(host):
     daemon_command = "python pd.py --name '%s' --host '%s'" % (env.host, host)
     nohup_wrapper = "(nohup %s &> /dev/null &) && true"
 
-    with cd("~/psim"), prefix("source env/bin/activate"):
+    with cd(PSTACK_DIR), prefix("source env/bin/activate"):
         run("killall -q python || true")  # terminate any existing daemons
         run(nohup_wrapper % daemon_command, pty=False)
 
